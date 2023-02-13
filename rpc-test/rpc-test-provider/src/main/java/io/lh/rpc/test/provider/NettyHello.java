5183f18144678b19fae36c42c5240aa563318fd6
Ling <992610900@qq.com>
2023-02-14 00:41:13 +0800

    最简单的一个netty server。

---
+++ rpc-test/rpc-test-provider/src/main/java/io/lh/rpc/test/provider/NettyHello.java
@@ -0,0 +1,24 @@
+package io.lh.rpc.test.provider;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyHello {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+
+                    }
+                });
+        serverBootstrap.bind(9090);
+    }
+}
