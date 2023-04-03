9b48983ccd301b4d09065bf943bcafa702c24f6e
lh <992610900@qq.com>
2023-04-03 23:52:04 +0800

    test

--- rpc-learn/rpc-learn-zookeeper/src/main/java/io/lh/rpc/Main.java
+++ rpc-learn/rpc-learn-zookeeper/src/main/java/io/lh/rpc/Main.java
@@ -1,7 +1,20 @@
 package io.lh.rpc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
 public class Main {
+
     public static void main(String[] args) {
+        Logger loggerFactory = LoggerFactory.getLogger(Main.class);
+
+        log.info("00");
+
+        loggerFactory.info("99");
+
         System.out.println("Hello world!");
     }
+
 }
