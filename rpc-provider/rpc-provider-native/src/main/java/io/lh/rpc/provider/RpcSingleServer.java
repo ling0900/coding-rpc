94d62a00506fbe64db16175f53200f7a860733b7
lh <992610900@qq.com>
2023-04-05 12:56:02 +0800

    修改 原声的启动类      RpcSingleServer

--- rpc-provider/rpc-provider-native/src/main/java/io/lh/rpc/provider/RpcSingleServer.java
+++ rpc-provider/rpc-provider-native/src/main/java/io/lh/rpc/provider/RpcSingleServer.java
@@ -20,16 +20,20 @@ public class RpcSingleServer extends BaseServer {
      * @param serviceAddress the service address
      * @param scanPackage    the scan package
      */
-    public RpcSingleServer(String serviceAddress, String scanPackage, String reflectType) {
+    public RpcSingleServer(String serviceAddress, String scanPackage, String reflectType,
+                           String registryAddress, String registryType) {
 
-        super(serviceAddress, reflectType);
+        super(serviceAddress, reflectType, registryType, reflectType);
 
         try {
-            this.handlerMap = RpcServiceProviderScanner.doScannerWithRpcReferenceAnnotationFilter(scanPackage);
+            this.handlerMap = RpcServiceProviderScanner.doScannerWithRpcServiceAnnotationFilterAndRegistryService(this.host, this.port, scanPackage, registryService);
+
             LOGGER.info("所有扫描到的服务有：\n");
+
             for (String key : this.handlerMap.keySet()) {
                 LOGGER.info("{}:\t{}", key, this.handlerMap.get(key));
             }
+
         } catch (Exception e) {
             LOGGER.info("RpcSingleServer异常{}", e);
         }
