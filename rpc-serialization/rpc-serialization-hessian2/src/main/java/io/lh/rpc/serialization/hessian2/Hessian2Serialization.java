92c0763321e3b3552f484cc00241b3c26f2c7bbf
lh <992610900@qq.com>
2023-04-16 20:17:02 +0800

    hessian2 序列化、反序列化

--- rpc-serialization/rpc-serialization-hessian2/src/main/java/io/lh/rpc/serialization/hessian2/Hessian2Serialization.java
+++ rpc-serialization/rpc-serialization-hessian2/src/main/java/io/lh/rpc/serialization/hessian2/Hessian2Serialization.java
@@ -1,10 +1,16 @@
 package io.lh.rpc.serialization.hessian2;
 
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
 import io.lh.rpc.commom.exception.SerializerException;
 import io.lh.rpc.serialization.api.Serialization;
 import io.lh.rpc.spi.annotation.SPIClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @SPIClass
 public class Hessian2Serialization implements Serialization {
@@ -19,14 +25,61 @@ public class Hessian2Serialization implements Serialization {
             throw new SerializerException("序列化出问题");
         }
 
+        byte[] result = new byte[0];
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Hessian2Output hessian2Output = new Hessian2Output(byteArrayOutputStream);
 
-        return new byte[0];
+        try {
+            hessian2Output.startMessage();
+            hessian2Output.writeObject(obj);
+            hessian2Output.flush();
+            hessian2Output.completeMessage();
+            result = byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            logger.error("序列化有问题{}", e);
+            throw new RuntimeException(e);
+        } finally {
+            if (hessian2Output != null) {
+                try {
+                    hessian2Output.close();
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public <T> T deserialize(byte[] data, Class<T> clazz) {
         logger.warn("执行 hessian2 反序列化");
 
-        return null;
+        if (data == null) {
+            throw new SerializerException("data is null");
+        }
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        Hessian2Input hessian2Input = new Hessian2Input(byteArrayInputStream);
+        T object = null;
+
+        try {
+            hessian2Input.startMessage();
+            object = (T) hessian2Input.readObject();
+            hessian2Input.completeMessage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (null != hessian2Input) {
+                try {
+                    hessian2Input.close();
+                    byteArrayInputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        return object;
     }
 }
---
+++ rpc-test/rpc-test-consumer/rpc-test-consumer-native/src/main/java/io/lh/rpc/test/consumer/RpcConsumerNativeSpiHession2Test.java
@@ -0,0 +1,76 @@
+package io.lh.rpc.test.consumer;
+
+import io.lh.rpc.constants.RpcConstants;
+import io.lh.rpc.consumer.RpcClient;
+import io.lh.rpc.proxy.api.async.IAsyncObjectProxy;
+import io.lh.rpc.proxy.api.future.RpcFuture;
+import io.lh.rpc.test.api.DemoService;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>描述：</p>
+ * <p>版本：1.0.0</p>
+ * <p>@author：lh</p>
+ * <p>创建时间：2023/02/27</p>
+ */
+public class RpcConsumerNativeSpiHession2Test {
+
+    /**
+     *
+     */
+    private RpcClient rpcClient;
+
+    /**
+     * json序列化类型
+     */
+    @Before
+    public void initRpcClient(){
+        rpcClient = new RpcClient("1.0.0",
+                "lh", 30000
+                , RpcConstants.SERIALIZATION_HESSIAN2
+                ,false, false
+                , "8.130.65.0:2181", "zookeeper");
+    }
+
+    /**
+     *
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcConsumerNativeSpiHession2Test.class);
+
+    /**
+     * Test interface rpc.
+     * 用于测试基于zookeeper为注册中心的
+     * @param args
+     */
+    @Test
+    public void testInterfaceRpc() {
+        DemoService demoService = rpcClient.create(DemoService.class);
+        String result = demoService.hello("lh");
+        LOGGER.info("封装后的返回数据" + result);
+        rpcClient.shutdown();
+    }
+
+    /**
+     * 异步测试 通过动态代理实现异步调用的测试类。
+     * 这次commit为止，如果生产者那里用到的cglib，这里就会出现问题！测试时候。
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void testAsyncInterfaceRpc() throws Exception {
+        // 这里的参数需要设置正确，否则会爆不存在的！
+//        rpcClient = new RpcClient("1.0.0", "lh", 3000, "cglib", false, false);
+        // 根据传入的接口，去服务提供者找对应的实现类（服务类）
+        IAsyncObjectProxy demoService = rpcClient.createAsync(DemoService.class);
+        // 直接调用方法就行了
+        RpcFuture future = demoService.call("hello", "qwe");
+        LOGGER.info("返回的数据===>>> " + future.get());
+        rpcClient.shutdown();
+    }
+
+    public static void main(String[] args) {
+    }
+}
