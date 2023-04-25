5ac8660284f2afa8f9d67832541f3147b5d5aabf
lh <992610900@qq.com>
2023-04-25 21:55:15 +0800

    心跳机制:缓存工具。

---
+++ rpc-consumer/rpc-consumer-common/src/main/java/io/lhrpc/consumer/common/cache/ConsumerChannelCache.java
@@ -0,0 +1,43 @@
+package io.lhrpc.consumer.common.cache;
+
+import io.netty.channel.Channel;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * The type Consumer channel cache.
+ *
+ * @author menglinghao
+ */
+public class ConsumerChannelCache {
+
+    private static volatile Set<Channel> channelCache = new CopyOnWriteArraySet<>();
+
+    /**
+     * Add.
+     *
+     * @param channel the channel
+     */
+    public static void add(Channel channel) {
+        channelCache.add(channel);
+    }
+
+    /**
+     * Remove.
+     *
+     * @param channel the channel
+     */
+    public static void remove(Channel channel) {
+        channelCache.remove(channel);
+    }
+
+    /**
+     * Gets channel cache.
+     *
+     * @return the channel cache
+     */
+    public static Set<Channel> getChannelCache() {
+        return channelCache;
+    }
+}
