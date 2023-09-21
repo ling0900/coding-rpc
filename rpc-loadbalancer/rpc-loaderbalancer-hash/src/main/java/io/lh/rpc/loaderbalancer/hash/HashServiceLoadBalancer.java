0ec26aa718b0c0257d0ab90777b32389a3b79f07
lh <992610900@qq.com>
2023-09-21 23:56:28 +0800

    SPI：hash负载均衡策略

---
+++ rpc-loadbalancer/rpc-loaderbalancer-hash/src/main/java/io/lh/rpc/loaderbalancer/hash/HashServiceLoadBalancer.java
@@ -0,0 +1,36 @@
+package io.lh.rpc.loaderbalancer.hash;
+
+import io.lh.rpc.loadbalancer.api.ServiceLoadBalancer;
+import io.lh.rpc.spi.annotation.SPIClass;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * The type Hash service load balancer.
+ *
+ * @author Ling
+ * @param <T> the type parameter
+ */
+@Slf4j
+@SPIClass
+public class HashServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    /**
+     * Select t.
+     *
+     * @param servers  the servers
+     * @param hashCode the hash code
+     * @return the t
+     */
+    @Override
+    public T select(List<T> servers, int hashCode) {
+        log.info("hash算法的负载均衡");
+
+        if (CollectionUtils.isEmpty(servers)) return null;
+
+        int index = Math.abs(hashCode) % servers.size();
+
+        return servers.get(index);
+    }
+}
